1e001b2275dbbd67baa0c315570800ed8d20f622
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-13 00:03:31 +0530

    fixed the routing bug

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail]       = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
@@ -17,8 +17,8 @@ const Login = () => {
         password,
       })
       .then((res) => {
-        if (res.data === "Success") navigate("/dashboard"); // redirect
-        else alert(res.data); // error msg
+        if (res.data === "Success") navigate("/dashboard");
+        else alert(res.data);
       })
       .catch(() => alert("Server error"));
   };
@@ -53,7 +53,7 @@ const Login = () => {
         </button>
 
         <p className="text-center mt-3 mb-0">
-          Don’t have an account? <a href="/register">Sign up</a>
+          Don’t have an account? <Link to="/register">Sign up</Link>
         </p>
       </form>
     </div>
